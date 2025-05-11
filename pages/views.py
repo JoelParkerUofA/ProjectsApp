3fae4c1fdfa98f4bcf51860f86b5d0b7bbb14df0
joelparkeruofa <joelparker@Joels-MacBook-Air.local>
2025-05-11 06:22:33 -0700

    making first app

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. We define the function that will render the html page. 
+# each veiw handles the logic the server processes each time the user visits a 
+# different url. 
+def home(request):
+    return(render(request, "pages/home.html",{}))
