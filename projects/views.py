00cbc808a4210f205807be527ab430cab66f12bb
joelparkeruofa <joelparker@joels-air.mynetworksettings.com>
2025-05-13 09:34:44 -0700

    adding projects to the protfolio website

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+
+# create index view
+def project_index(request):
+    projects = Project.objects.all() # reteive all instances in projects table
+    context = {
+        "projects": projects # create a dictionary with project information
+    }
+    return render(request, "projects/project_index.html", context) # renders index.html with the information from the projects
+
+
+# Create detail veiw which utilizes a key to make sure the correct project is rendered. 
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
